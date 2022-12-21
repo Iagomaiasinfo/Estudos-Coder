8ed396cf872e73836b6780b58cf1dfe63595b227
Iago Maia <iagomaiasinfo@gmail.com>
2022-12-20 22:29:11 -0300

    teste não finalizado

---
+++ Exercicios_web/html/exercicios/js/scripts.js
@@ -0,0 +1,33 @@
+var btn = document.querySelector('#show-or-hide');
+var btn2 = document.querySelector('#hidis');
+var btn3 = document.querySelector('.tree');
+
+btn.addEventListener('click', function() {
+    
+    if(show-or-hide.style.display === 'block') {
+        show-or-hide.style.display = 'none';
+    } else {
+        show-or-hide.style.display = 'block';
+    }
+
+});
+
+btn2.addEventListener('click', function(){
+    
+    if(hidis.style.display === 'block') {
+        hidis.style.display = 'none';
+    } else {
+        hidis.style.display = 'block';
+    }
+
+});
+
+btn3.addEventListener('click', function(){
+    
+    if(tree.style.display === 'block') {
+        tree.style.display = 'none';
+    } else {
+        tree.style.display = 'block';
+    }
+
+});
